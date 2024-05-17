e186f95cd599c99d680ae737a5f005bcd7e142dc
Azpect3120 <104033825+Azpect3120@users.noreply.github.com>
2024-05-17 14:22:53 -0700

    INIT: Exercises

---
+++ Exercises/bin/main.ml
@@ -0,0 +1,98 @@
+let () = Printf.printf "%s\n" "Ocaml Exercises. https://ocaml.org/exercises";;
+
+
+
+(* Tail of a List *)
+(* Write a function last : 'a list -> 'a option that returns the last element of a list *)
+let rec last lst = 
+  match lst with
+  | [] -> None
+  | [el] -> Some el
+  | _ :: tail -> last tail
+;;
+
+(* Test with valid list *)
+(* Should print "Last element: 8" *)
+let last_list_1 = [1; 2; 3; 4; 5; 6; 7; 8];;
+Printf.printf "Last element: %s\n" (
+  match (last last_list_1) with 
+  | None -> "Empty list"
+  | Some el -> string_of_int el
+);;
+
+(* Test with invalid list *)
+(* Should print "Last element: Empty list" *)
+let last_list_2 = [];;
+Printf.printf "Last element: %s\n" (
+  match (last last_list_2) with 
+  | None -> "Empty list"
+  | Some el -> string_of_int el
+);;
+
+
+
+(* Last Two Elements of a List  *)
+(* Find the last but one (last and penultimate) elements of a list. *)
+let rec last_two lst = 
+  match lst with
+  | [] | [_] -> None
+  | [x; y] -> Some [x; y]
+  | _ :: tail -> last_two tail
+;;
+
+(* Test with a valid list *)
+(* Should print "Last 2 elements: 4 5" *)
+let last_two_list_1 = ["1"; "2"; "3"; "4"; "5"];;
+Printf.printf "Last 2 elements: %s\n" (
+  match (last_two last_two_list_1) with 
+  | None -> "Length is less than 2"
+  | Some lst -> (String.concat " " lst)
+);;
+
+(* Test with a invalid list (empty) *)
+(* Should print "Last 2 elements: Length is less than 2" *)
+let last_two_list_2 = [];;
+Printf.printf "Last 2 elements: %s\n" (
+  match (last_two last_two_list_2) with 
+  | None -> "Length is less than 2"
+  | Some lst -> (String.concat " " lst)
+);;
+
+(* Test with a invalid list (one element) *)
+(* Should print "Last 2 elements: Length is less than 2" *)
+let last_two_list_3 = ["1"];;
+Printf.printf "Last 2 elements: %s\n" (
+  match (last_two last_two_list_3) with 
+  | None -> "Length is less than 2"
+  | Some lst -> (String.concat " " lst)
+);;
+
+
+
+(* N'th Element of a List  *)
+(* Find the N'th element of a list. *)
+let rec nth lst n = 
+  match lst with 
+  | [] -> None
+  | head :: tail -> match n with 
+    | 0 -> Some head
+    | _ -> nth tail (n - 1)
+;;
+
+(* Test with valid list *)
+(* Should print "Element at index 4 is 5" *)
+let nth_list_1 = ["1"; "2"; "3"; "4"; "5"; "6"; "7"; "8"];;
+Printf.printf "Element at index 4 is %s\n" (
+  match (nth nth_list_1 4) with
+  | None -> "does not exist"
+  | Some el -> el
+);;
+
+(* Test with invalid list *)
+(* Should print "Element at index 4 does not exist" *)
+let nth_list_2 = [];;
+Printf.printf "Element at index 4 is %s\n" (
+  match (nth nth_list_2 4) with
+  | None -> "does not exist"
+  | Some el -> el
+);;
